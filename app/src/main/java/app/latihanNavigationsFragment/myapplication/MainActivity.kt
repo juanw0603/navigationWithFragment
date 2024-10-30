43d45cafeee551dee38a82abf6018a643edc61d2
juanw0603 <c14210290@john.petra.ac.id>
2024-10-30 19:11:11 +0700

    update : update pada bagian MainActivity.kt

--- app/src/main/java/app/latihanNavigationsFragment/myapplication/MainActivity.kt
+++ app/src/main/java/app/latihanNavigationsFragment/myapplication/MainActivity.kt
@@ -19,9 +19,12 @@ class MainActivity : AppCompatActivity() {
         }
         val mFragmentManager = supportFragmentManager
         val Fhalaman1 = halaman1()
-
+        val Fhalaman2 = halaman2()
+        val Fhalaman3 = halaman3()
 
         val _navToHalaman1 = findViewById<Button>(R.id.halaman1)
+        val _navToHalaman2 = findViewById<Button>(R.id.halaman2)
+        val _navToHalaman3 = findViewById<Button>(R.id.halaman3)
 
         _navToHalaman1.setOnClickListener {
             mFragmentManager.findFragmentByTag(halaman1::class.java.simpleName)
@@ -30,6 +33,15 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.FragmentPlace, Fhalaman1,halaman1::class.java.simpleName)
                 .commit()
         }
+
+        _navToHalaman2.setOnClickListener {
+            mFragmentManager.findFragmentByTag(halaman2::class.java.simpleName)
+            mFragmentManager
+                .beginTransaction()
+                .replace(R.id.FragmentPlace, Fhalaman2,halaman2::class.java.simpleName)
+                .commit()
+        }
+
 
     }
 }
